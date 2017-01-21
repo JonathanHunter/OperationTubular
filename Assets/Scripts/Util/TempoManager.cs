9c4234607d01222cfee94a8be713e362a164356f
nshooter3 <nick.shooter@gmail.com>
2017-01-21 05:38:36 -0500

    added tempo manager

---
+++ Assets/Scripts/Util/TempoManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Util
+{
+    public class TempoManager : MonoBehaviour
+    {
+
+
+
+        public int bpm = 120;
+        public float beat, timer;
+
+        // Use this for initialization
+        void Start()
+        {
+            SetBeat(bpm);
+        }
+
+        public void SetBeat(int bpm)
+        {
+            this.bpm = bpm;
+            beat = (60f / bpm);
+            timer = beat;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                Beat();
+                timer = beat;
+            }
+        }
+
+        public void Beat()
+        {
+            //TODO PUT DA CODE HERE JONNY BOY
+        }
+    }
+}
