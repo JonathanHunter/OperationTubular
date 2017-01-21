72f950aea5defe946b556937170cd3db1078b102
Jonathan Hunter <hunterjonathant@gmail.com>
2017-01-21 01:14:24 -0500

    Generator code

---
+++ Assets/Scripts/Generation/Wave.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Generation
+{
+    class Wave : MonoBehaviour
+    {
+        public WaveUnit[] units;
+
+        public void Spawn()
+        {
+            foreach(WaveUnit u in units)
+            {
+                GameObject obj = Instantiate(u.unit);
+                obj.transform.position = new Vector3(u.transform.localPosition.x, u.transform.localPosition.y, u.transform.localPosition.z);
+            }
+        }
+    }
+}
