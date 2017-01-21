0b3ad066d669b2a820dafd361be0ed9435955cf0
Jonathan Hunter <hunterjonathant@gmail.com>
2017-01-20 22:02:02 -0500

    random number

--- Assets/Scripts/Util/SinusoidalRandom.cs
+++ Assets/Scripts/Util/SinusoidalRandom.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using UnityEngine;
 
 namespace Assets.Scripts.Util
 {
-    class SinusoidalRandom
+    class SinusoidalRandom :MonoBehaviour
     {
+        private static bool isDay;
+        private static bool inited;
+
+        private void Start()
+        {
+            if (!inited)
+                Init();
+        }
+
+        private static void Init()
+        {
+            Random.InitState(System.DateTime.Now.Millisecond);
+            inited = true;
+            isDay = System.DateTime.Now.Hour < 12;
+        }
+
+        public static float Range(float start, float end)
+        {
+            if (!inited)
+                Init();
+            float r = Random.Range(start, end);
+            if(isDay)
+                r = (Mathf.Sin(r) % end) + start;
+            else
+                r = (Mathf.Cos(r) % end) + start;
+            return r;
+        }
     }
 }
