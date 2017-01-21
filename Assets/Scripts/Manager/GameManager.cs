b835725a965a8361415c1e9505490e45879d4b48
Jonathan Hunter <hunterjonathant@gmail.com>
2017-01-21 03:11:48 -0500

    Manager

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Manager
+{
+    class GameManager : MonoBehaviour
+    {
+        private static bool paused;
+
+        public static bool IsPaused { get { return paused; } }        
+    }
+}
