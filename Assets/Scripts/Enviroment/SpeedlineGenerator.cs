203fbc38165d9b22d7664a10ae922227e48f0212
Spex130 <Gamerguy56@bellsouth.net>
2017-01-21 10:57:41 -0500

    Speedline basis
    UNFINISHED.

---
+++ Assets/Scripts/Enviroment/SpeedlineGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedlineGenerator : MonoBehaviour {
+
+    public SpeedlineObject linePic;
+    private SpeedlineObject temp;
+
+    public bool shouldGenerate = false;
+
+	// Use this for initialization
+	void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(shouldGenerate)
+             generateLine();
+    }
+
+    public void generateLine()
+    {
+        temp = Instantiate<SpeedlineObject>(linePic);
+        linePic.myParent = this;
+        temp.transform.position = new Vector3(Random.Range(4, 6), Random.Range(4, 6), Random.Range(4, 6));
+    }
+}
---
+++ Assets/Scripts/Enviroment/SpeedlineObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedlineObject : MonoBehaviour {
+
+    public SpeedlineGenerator myParent;
+    public float reduceSpeed = .2f;
+
+    public SpeedlineObject(SpeedlineGenerator parent)
+    {
+        myParent = parent;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotate();
+        reduce();
+	}
+
+    public void rotate()
+    {
+        Quaternion rotation = Quaternion.LookRotation(myParent.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles);
+    }
+
+    public void reduce()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - reduceSpeed, transform.localScale.z);
+        if (transform.localScale.y <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
