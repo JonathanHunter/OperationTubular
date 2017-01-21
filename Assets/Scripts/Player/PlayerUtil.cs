958807b0a4e094595cc1bfc89b3397035e041fa9
neonwednesdays <dxiao.ns@gmail.com>
2017-01-21 02:48:09 -0500

    Refactor Player movement handling

--- Assets/Scripts/Player/PlayerUtil.cs
+++ Assets/Scripts/Player/PlayerUtil.cs
@@ -4,6 +4,7 @@ using System.Collections;
 namespace Assets.Scripts.Player {
 	class PlayerUtil : MonoBehaviour {
 		/*
+			returns unity values of rotation
 			@params currentRotation - pass in the z rotation
 			@return float - returns rotation assuming 0 is straight and 
 							left is negative and 
@@ -17,6 +18,20 @@ namespace Assets.Scripts.Player {
 				return -Mathf.Abs(currentRotation);
 			}
 			return 0;
+		}
+
+		/*
+			returns how far into the bobbing rotation it is in
+			@params currentRotation - pass in the z rotation
+			@params maxRotation - pass in the max rotation
+			@return float - returns between -1 and 1
+
+		*/
+		public static float getPercentInRotation(float currentRotation, float maxRotation) {
+			float rotationValue = PlayerUtil.getRelativeRotation(currentRotation);
+			float direction = rotationValue > 0 ? 1 : -1;
+			float absPercent = Mathf.Abs(rotationValue) / maxRotation;
+			return absPercent * direction;
 		}
 	}
 }
