0976bb64abfaa01e72d5d1132a38bd5d1dda99b7
Spex130 <Gamerguy56@bellsouth.net>
2017-01-22 10:46:30 -0500

    Menu/Title changes

---
+++ Assets/LoadOnInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Util;
+using Assets.Scripts.Manager;
+
+
+public class LoadOnInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (InputManager.instance.startPressed1 || InputManager.instance.startPressed2 || Input.GetKeyUp(KeyCode.Return))
+        {
+            GameManager.instance.LoadMenu();
+        }
+	}
+}
