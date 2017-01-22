0f8a6d119792b41ddfadd8fa379031dbd00ada43
nshooter3 <nick.shooter@gmail.com>
2017-01-21 22:40:30 -0500

    added day night transitions

---
+++ Assets/Scripts/Enviroment/NightManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Environment
+{
+    public class NightManager : MonoBehaviour
+    {
+        public static NightManager instance;
+        public SpriteRenderer night, nightOcean;
+        public ParticleSystem rain;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                instance = this;
+            }
+            else if (this != instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            rain.Stop();
+            night = GetComponent<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        //Call this to trigger night
+        public void ToNight()
+        {
+            StartCoroutine(FadeNightIn());
+            rain.Play();
+        }
+
+
+        //Call this to trigger day
+        public void ToDay()
+        {
+            StartCoroutine(FadeNightOut());
+            rain.Stop();
+        }
+
+        private IEnumerator FadeNightIn()
+        {
+            for (float i = 0f; i < 1f; i += 0.05f)
+            {
+                night.color = new Color(night.color.r, night.color.g, night.color.b, i);
+                nightOcean.color = new Color(night.color.r, night.color.g, night.color.b, i);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+
+        private IEnumerator FadeNightOut()
+        {
+            for (float i = 1f; i > 0f; i -= 0.05f)
+            {
+                night.color = new Color(night.color.r, night.color.g, night.color.b, i);
+                nightOcean.color = new Color(night.color.r, night.color.g, night.color.b, i);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+
+
+
+    }
+}
