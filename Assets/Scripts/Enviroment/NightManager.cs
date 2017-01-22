b744a812259b94ce95b211be8f3c0822a3389fda
nshooter3 <nick.shooter@gmail.com>
2017-01-22 04:51:19 -0500

    fixed layering issue with night ocean

--- Assets/Scripts/Enviroment/NightManager.cs
+++ Assets/Scripts/Enviroment/NightManager.cs
@@ -28,6 +28,7 @@ namespace Assets.Scripts.Environment
         {
             rain.Stop();
             night = GetComponent<SpriteRenderer>();
+            //ToNight();
         }
 
         // Update is called once per frame
@@ -56,7 +57,7 @@ namespace Assets.Scripts.Environment
             for (float i = 0f; i < 1f; i += 0.05f)
             {
                 night.color = new Color(night.color.r, night.color.g, night.color.b, i);
-                nightOcean.color = new Color(night.color.r, night.color.g, night.color.b, i);
+                nightOcean.color = new Color(nightOcean.color.r, nightOcean.color.g, nightOcean.color.b, i);
                 yield return new WaitForSeconds(0.1f);
             }
         }
