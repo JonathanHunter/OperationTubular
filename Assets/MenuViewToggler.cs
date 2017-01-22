c3bc71d6927b6dbcaad2cab78bc0463f1a17a669
Spex130 <Gamerguy56@bellsouth.net>
2017-01-22 12:29:24 -0500

    Improved menu
    Contains Credits, includes instructions.

---
+++ Assets/MenuViewToggler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuViewToggler : MonoBehaviour {
+
+    public GameObject mainView;
+    public GameObject creditsView;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void viewMain()
+    {
+        creditsView.SetActive(false);
+        mainView.SetActive(true);
+    }
+
+    public void viewCredits()
+    {
+        creditsView.SetActive(true);
+        mainView.SetActive(false);
+    }
+}
